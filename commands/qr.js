b4fa2e88474cd69d8238280b83e4b5f8c8f206df
nebbilies <hirohamada17092005@gmail.com>
2025-10-05 17:25:51 +0700

    feat: add QR code command for payment in Discord

---
+++ commands/qr.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const qrImagePath = 'assets/qr.jpg';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('qr')
+        .setDescription('Get the QR code for payment'),
+    async execute(interaction) {
+        await interaction.reply({
+            content: '📬 QR chuyển khoản đã được gửi trong DM!',
+            ephemeral: true,
+        });
+        const channel = await interaction.user.createDM();
+        await channel.send({
+            content: 'QR để bạn chuyển khoản thanh toán nè~! Cảm ơn bạn đã ủng hộ VCL <3',
+            files: [qrImagePath],
+        });
+    },
+};
